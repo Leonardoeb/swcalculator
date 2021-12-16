927e36bcf8542775fd52a7431830c21662b4ea46
03598181108 <leonardomatos222@gmail.com>
2021-12-16 11:21:40 -0300

    rename

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Distance from './pages';
+import Starship from './pages';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Distance />
+    <Starship />
   </React.StrictMode>,
   document.getElementById('root')
 );
